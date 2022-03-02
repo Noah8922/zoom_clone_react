5aac8bee13baba90f7ca88907c628c1b37fd8901
Noah8922 <juyeolgo@gmail.com>
2022-03-02 15:36:28 +0900

    Front-end setup with material UI

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+
+//import component
+import Videoplayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notification from "./components/Notification";
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <Videoplayer />
+      <Options>
+        <Notification />
+      </Options>
+    </div>
+  );
+};
+
+export default App;
