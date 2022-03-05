cbcba1728c48888d15e58858c4ca402111185220
Noah8922 <juyeolgo@gmail.com>
2022-03-05 10:07:16 +0900

    get stream of peers

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -7,7 +7,7 @@ import { history } from "../redux/configureStore";
 import styled from "styled-components";
 
 const Videoplayer = (props) => {
-  const roomId = props.match.params.roomName;
+  const roomName = props.match.params.roomName;
   const [me, setMe] = useState("");
   const [muted, setMuted] = useState(false);
   const [cameraOff, setCameraOff] = useState(false);
@@ -15,13 +15,15 @@ const Videoplayer = (props) => {
   const [video, setVideo] = useState([]);
 
   const videoGrid = useRef();
-  const myVideo = useRef();
   const muteBtn = useRef();
   const cameraBtn = useRef();
   const leaveBtn = useRef();
   const cameraSelect = useRef();
   const call = useRef();
   const mystream = useRef();
+  const myVideo = useRef();
+  const userstream = useRef();
+  const userVidoe = useRef();
 
   let myStream;
   let myPeerConnection;
@@ -34,8 +36,7 @@ const Videoplayer = (props) => {
       .then((stream) => {
         myStream = stream;
         let streamId = stream.id;
-        console.log(myStream, streamId);
-        socket.emit("join-room", roomId);
+        socket.emit("join-room", roomName);
         addVideoStream(myVideo.current, stream);
         videoGrid.current.append(myVideo.current);
         getCameras();
@@ -47,7 +48,6 @@ const Videoplayer = (props) => {
     try {
       const devieces = await navigator.mediaDevices.enumerateDevices();
       const cameras = devieces.filter((device) => device.kind === "videoinput");
-      console.log(cameras[0].deviceId);
       cameras.forEach((camera) => {
         const option = document.createElement("option");
         option.value = cameras[0].deviceId;
@@ -63,13 +63,45 @@ const Videoplayer = (props) => {
     myPeerConnection = new RTCPeerConnection(); //peer to pper connection
     setAudio(myStream.getTracks().filter((track) => track.kind === "audio"));
     setVideo(myStream.getTracks().filter((track) => track.kind === "video"));
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("track", AddStream);
     myStream
       .getTracks()
       .forEach((track) => myPeerConnection.addTrack(track, myStream));
-    console.log(myPeerConnection);
-    socket.on("welcome", () => {
-      console.log("someone joined");
-    });
+  }
+
+  //방만든 브라우저에서 일어나는 일
+  socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    await myPeerConnection.setLocalDescription(offer);
+    // console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
+  });
+
+  // 이후 참가한 방에 일어나는 일
+  socket.on("offer", async (offer) => {
+    // console.log("received the offer");
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    await myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    // console.log("sent the answer");
+  });
+
+  //방 만든 브라우저에서 일어나는 일 (참가한 방에서 보낸 answer을 받아 저장함.)
+  socket.on("answer", (answer) => {
+    // console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
+  });
+
+  socket.on("ice", async (ice) => {
+    // console.log("received candidate");
+    await myPeerConnection.addIceCandidate(ice);
+  });
+  //////////////////////////////////////
+  function handleIce(data) {
+    // console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
   }
 
   function handleMuteClick() {
@@ -94,6 +126,11 @@ const Videoplayer = (props) => {
     }
   }
 
+  function AddStream(data) {
+    // console.log("let's see this is work");
+    console.log(data.track);
+  }
+
   function LeaveRoom() {
     history.push("/");
     window.location.reload();
@@ -105,6 +142,23 @@ const Videoplayer = (props) => {
         <div ref={mystream}>
           <div ref={videoGrid} id="video-grid">
             <video ref={myVideo} autoPlay playsInline></video>
+          </div>
+          <div>
+            <button ref={muteBtn} onClick={handleMuteClick}>
+              mute
+            </button>
+            <button ref={cameraBtn} onClick={handleCameraClick}>
+              camera
+            </button>
+            <button ref={leaveBtn} onClick={LeaveRoom}>
+              leave
+            </button>
+            <select ref={cameraSelect}></select>
+          </div>
+        </div>
+        <div ref={userstream}>
+          <div ref={videoGrid} id="video-grid">
+            <video ref={userVidoe} autoPlay playsInline></video>
           </div>
           <div>
             <button ref={muteBtn} onClick={handleMuteClick}>
--- server.js
+++ server.js
@@ -22,6 +22,15 @@ io.on("connection", (socket) => {
     socket.join(roomName);
     socket.to(roomName).emit("welcome");
   });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
